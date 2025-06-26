40d2d1ee363d1d67ee39e86641926329287f21ba
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-26 20:35:19 +0530

    feat: storing the record from form to database

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,17 @@ route.get("/", (req, res) => {
 route.post("/", (req, res) => {
   const { name, m1, m2, m3, m4 } = req.body;
   const sql = `INSERT INTO marks(sname, marks1, marks2, marks3) VALUES (?, ?, ?, ?);`;
+
+  pool.query(sql, [name, m1, m2, m3, m4], (err, results, fields) => {
+    if (err) {
+      console.error(
+        "Insertion failed due to the following error: ",
+        err.message
+      );
+      return res.status(500).send("Record not inserted");
+    }
+    console.log("Record inserted successfully");
+  });
 });
 
 module.exports = route;
